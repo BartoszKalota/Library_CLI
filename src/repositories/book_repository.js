e5b62d02a4f8b42e103126a060cf192e4fc8205b
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-26 16:53:43 +0200

    Save a new book

--- src/repositories/book_repository.js
+++ src/repositories/book_repository.js
@@ -1,28 +1,51 @@
 import path from 'path';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-console.log(uuidv4());
+
+const pathToBooks = path.resolve('./src/assets/books');
 
 const getBooksCollection = () => {
-  const pathToBooks = path.resolve('./src/assets/books');
   return JSON.parse(fs.readFileSync(pathToBooks, 'utf-8'));
 };
 
 export const bookRepository = {
   findAll: async () => {
-    const booksCollection = await getBooksCollection();
-    return Object.values(booksCollection);
+    try {
+      const booksCollection = await getBooksCollection();
+      return Object.values(booksCollection);
+    } catch (err) {
+      console.log(err);
+    }
   },
 
   findById: async (id) => {
-    const booksCollection = await getBooksCollection();
-    return booksCollection[id];
+    try {
+      const booksCollection = await getBooksCollection();
+      return booksCollection[id];
+    } catch (err) {
+      console.log(err);
+    }
   },
 
   save: async (book) => {
-    const booksCollection = await getBooksCollection();
-
+    try {
+      const booksCollection = await getBooksCollection();
+      const signedBook = {
+        [`${book.id}`]: { ...book }
+      };
+      Object.assign(booksCollection, signedBook);
+      await fs.promises.writeFile(pathToBooks, JSON.stringify(booksCollection, undefined, 2));
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
-bookRepository.save();
+const newBook = {
+  id: uuidv4(),
+  title: 'New Title',
+  author: 'New Author',
+  genre: 'comedy',
+  path: './somePath/newBook.pdf'
+};
+bookRepository.save(newBook);
