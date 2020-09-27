ca9a0399abb33155676f87995c4f033255372f94
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-27 16:41:25 +0200

    BookId route handling

--- src/client-server.js
+++ src/client-server.js
@@ -2,14 +2,14 @@ import http from 'http';
 
 import ENVS from './config.js';
 import { bookRepository } from './repositories/book_repository.js';
+import { getBook } from './services/getBook.js';
 
 const getBooksURL = '/book';
 const getBookByIdURL = '/book?bookId=';
 
 const server = http.createServer(async (req, res) => {
   try {
-    const { method, url } = req;
-    const endpointSupported = method === 'GET' && url.includes(getBooksURL);
+    const endpointSupported = req.method === 'GET' && req.url.includes(getBooksURL);
 
     if (!endpointSupported) {
       res.statusCode = 404;
@@ -17,10 +17,26 @@ const server = http.createServer(async (req, res) => {
       return;
     }
 
-    if (url === getBooksURL) {
+    if (req.url === getBooksURL) {
       const booksCollection = await bookRepository.findAll();
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(booksCollection, undefined, 2));
+    }
+
+    if (req.url.includes(getBookByIdURL)) {
+      const bookId = req.url.replace(getBookByIdURL, '');
+
+      if (!bookId || !req.headers.accept) {
+        res.statusCode = 400;
+        res.end('BookId or Accept missing.');
+      }
+
+      const fetchedCertainBook = await getBook(bookId);
+      if (!fetchedCertainBook) {
+        res.statusCode = 404;
+        res.end('Book not found.');
+        return;
+      }
     }
 
     res.end();
---
+++ src/services/getBook.js
@@ -0,0 +1,3 @@
+export const getBook = (id) => {
+  
+};
