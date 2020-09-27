86e44bcc3d788bf0f15a49c927aee5b71f81d9d0
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-27 21:13:40 +0200

    Handler for the command of deleting book

--- src/services/deleteBook.js
+++ src/services/deleteBook.js
@@ -1,3 +1,11 @@
-export const deleteBook = () => {
-  console.log('test');
+import inquirer from 'inquirer';
+import { bookRepository } from '../repositories/book_repository.js';
+const { prompt } = inquirer;
+
+export const deleteBook = async () => {
+  const { id } = await prompt([
+    { name: 'id', message: 'What is the ID of the book to delete?' }
+  ]);
+
+  await bookRepository.delete(id);
 };
