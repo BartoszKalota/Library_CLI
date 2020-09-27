b6085edffb3bae76f33e2d1552ba919ac62db7c2
Bartosz Kalota <bartek.kalota@wp.pl>
2020-09-27 18:41:36 +0200

    Envs error msg

--- src/config.js
+++ src/config.js
@@ -3,6 +3,10 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve('./src/.env') });
 
+if (!process.env.NODE_PORT || !process.env.ACCEPTED_GENRES) {
+  throw new Error('Missing environmental variable.');
+}
+
 export default {
   NODE_PORT: process.env.NODE_PORT,
   ACCEPTED_GENRES: process.env.ACCEPTED_GENRES
